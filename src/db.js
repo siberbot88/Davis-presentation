0af1fade735922c0d9ceb13452c32a41f0be98dd
Mohammad Bayu Rizki <widjanarko020@gmail.com>
2026-05-22 12:05:16 +0700

    Support MySQL environment variable aliases

--- src/db.js
+++ src/db.js
@@ -4,11 +4,11 @@ import mysql from "mysql2/promise";
 dotenv.config();
 
 export const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT || 3306),
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: process.env.DB_HOST || process.env.MYSQLHOST,
+  port: Number(process.env.DB_PORT || process.env.MYSQLPORT || 3306),
+  user: process.env.DB_USER || process.env.MYSQLUSER,
+  password: process.env.DB_PASSWORD || process.env.MYSQLPASSWORD,
+  database: process.env.DB_NAME || process.env.MYSQLDATABASE,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
